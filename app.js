d081d0d30b95e76af14fbab3cf0f0f1f10d474c2
Dmitry Khrustalev <xryctdm@gmail.com>
2020-04-16 20:17:45 +0300

    разработка сервера place

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const routeCards = require('./routes/cards.js');
+const routeUsers = require('./routes/users.js');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routeCards);
+app.use('/', routeUsers);
+app.use('/', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`listening on port ${PORT}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const users = require('../data/users.json');
+
+router.get('/users', (req, res) => {
+  res.send(users);
+});
+
+const doesUserExist = (req, res, next) => {
+  const initialUser = users.find((user) => user._id === req.params.id);
+  if (!initialUser) {
+    res.status(404).send({ message: 'Нет пользователя с таким id' });
+    return;
+  }
+  req.user = initialUser;
+  next();
+};
+
+router.get('/users/:id', doesUserExist, (req, res) => {
+  res.send(req.user);
+});
+
+module.exports = router;
