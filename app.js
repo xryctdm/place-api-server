71d2be82a6b6cb362fac23fa9eb712d303401fe4
Dmitry Khrustalev <xryctdm@gmail.com>
2020-04-17 17:01:47 +0300

    работа над ошибками

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 const routeCards = require('./routes/cards.js');
 const routeUsers = require('./routes/users.js');
@@ -9,14 +8,11 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routeCards);
 app.use('/', routeUsers);
-app.use('/', (req, res) => {
+app.use('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
