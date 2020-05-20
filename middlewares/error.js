e33d07b32f9d969d1f8e84e1c0a1438489d689f2
Dmitry Khrustalev <xryctdm@gmail.com>
2020-05-20 14:58:31 +0300

    fix

--- middlewares/error.js
+++ middlewares/error.js
@@ -4,5 +4,4 @@ module.exports = (err, req, res, next) => {
 
   res
     .status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
-    console.log('fvdfv');
 };
